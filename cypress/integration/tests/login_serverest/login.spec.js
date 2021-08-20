3b9807254bfb28fd82a8b7af4897a92464d112ff
MichaelBSBarros <75203020+MichaelBSBarros@users.noreply.github.com>
2021-08-20 11:21:47 -0300

    configuração de ambiente

---
+++ cypress/integration/tests/login_serverest/login.spec.js
@@ -0,0 +1,13 @@
+/// <reference types ="cypress"/>
+
+describe('Realizar login', () => {
+    context('Dado que esteja na pagina home do site sauce.demo', () => {
+
+            it(`Quando preencher os campos com dados do tipo`, () => {
+                
+                cy.visit('/')
+         
+        }) 
+    })
+           
+})    
